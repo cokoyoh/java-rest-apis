81cb66514b61481c6e6f12a0f9bdb6696bf8a21b
cokoyoh <charlesokoyoh@gmail.com>
2019-12-17 20:47:54 +0300

    Initialise a restful api springboot application

---
+++ src/test/java/com/springbootrestful/restfulapis/RestfulApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrestful.restfulapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
