2fb16aa7f160f399489e3be4fbe7d4363284a033
cokoyoh <charlesokoyoh@gmail.com>
2019-12-18 10:33:44 +0300

    Getting a converted message form a java bean

--- src/main/java/com/springbootrestful/restfulapis/home/HomeController.java
+++ src/main/java/com/springbootrestful/restfulapis/home/HomeController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String welcome() {
+        return "Welcome to SpringBoot";
+    }
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
         return "Hello World!";
+    }
+
+    @GetMapping(path = "hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Some string bean");
     }
 }
