89548a5fd5525482d6d461f011ac852ff36cc3d6
cokoyoh <charlesokoyoh@gmail.com>
2019-12-18 10:50:16 +0300

    Using path variables

--- src/main/java/com/springbootrestful/restfulapis/home/HomeController.java
+++ src/main/java/com/springbootrestful/restfulapis/home/HomeController.java
@@ -1,6 +1,7 @@
 package com.springbootrestful.restfulapis.home;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +19,10 @@ public class HomeController {
     @GetMapping(path = "hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Some string bean");
+    }
+
+    @GetMapping(path = "hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World %s", name));
     }
 }
