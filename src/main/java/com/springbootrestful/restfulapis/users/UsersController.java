3dda26284d6489122e52896af02624e2bac7c5d4
cokoyoh <charlesokoyoh@gmail.com>
2019-12-19 09:40:50 +0300

    Add a user resource to store user and return the location of the created user

--- src/main/java/com/springbootrestful/restfulapis/users/UsersController.java
+++ src/main/java/com/springbootrestful/restfulapis/users/UsersController.java
@@ -1,8 +1,41 @@
 package com.springbootrestful.restfulapis.users;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 public class UsersController {
 
+    @Autowired
+    private UserDaoService service;
+
+    @GetMapping(path = "/users")
+    public List<User> index() {
+        return service.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User show(@PathVariable int id) {
+        //what id the user is not available????
+
+        return service.findOne(id);
+    }
+
+    @PostMapping(path = "/users")
+    public ResponseEntity<Object> store(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 }
