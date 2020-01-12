6c390ddc9d015ee8aa8caf91e5795c7813f2b622
cokoyoh <charlesokoyoh@gmail.com>
2020-01-12 12:36:23 +0300

    Change the user bean into a jpa entity

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(1, sysdate(), 'John Doe')
+insert into user values(2, sysdate(), 'Jane Doe')
+insert into user values(3, sysdate(), 'Roe Smith')
+insert into user values(4, sysdate(), 'Wade Wilson')
+insert into user values(5, sysdate(), 'Filadora Figg')
